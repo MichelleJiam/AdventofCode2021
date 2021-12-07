6d569b1ca7a3b23fd6c4757520631e8df7531688
mjiam <mjiam@student.codam.nl>
2021-12-07 12:53:22 +0100

    Cleaned up a little, still not optimal

--- D04/d04b.py
+++ D04/d04b.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python
 from itertools import chain
-
-def sumCard(card):
-	sumC = sum([int(n) for n in card if n is not 'X'])
-	return sumC
-
-
-def	checkBingo(card, n):
-	# horizontal check
-	for row in zip(*[iter(card)]*5):
-		if all(item=='X' for item in row):
-			return True
-		# [x]*n = a list of n amounts of x. [iter(s)*5] passes the same iterator 5 times to zip and zip dereferences that iterator each time.
-
-	# vertical check
-	for column in zip(card, card[5:], card[10:], card[15:], card[20:]):
-		if all(item=='X' for item in column):
-			return True
-
-def crossCommon(cards, n):	
-	for card in cards:
-		if n in card:
-			found = card.index(n)
-			card[found] = 'X'
-	return cards
+from d04a import checkBingo, crossCommon
 
 with open('d04.in') as f:
 	draws = [n.strip() for n in next(f).split(",")]
@@ -40,6 +17,5 @@ for n in draws:
 			break;
 	else: continue
 	break
-print(winners)
-finalScore = sumCard(cards[winners[-1]]) * int(n)
-print(finalScore)
+sumCard = sum([int(c) for c in cards[winners[-1]] if c is not 'X'])
+print('Final score is: ', sumCard * int(n))
